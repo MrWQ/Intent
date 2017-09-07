cdf7b88bf0080f432ed9c6967036b06671e64be2
MrWQ <1061563931@qq.com>
2017-09-07 16:27:13 +0800

    Intent实践： 两个Activity互传数据&隐式启动和显示启动

--- app/src/main/java/com/example/administrator/intenttest/MainActivity.java
+++ app/src/main/java/com/example/administrator/intenttest/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,10 +17,23 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent();
+                Intent intent = new Intent();//intent 隐式启动
                 intent.setAction("android.intent.action.VIEW");
-                startActivity(intent);
+                intent.putExtra("name", "Wq");
+                intent.putExtra("age", 20);
+                intent.putExtra("tel", "1361109986");
+                startActivityForResult(intent, 0);//设置请求码 和数据
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == 0) {//严谨的话：if(requestCode==0 &&resultCode ==0)
+            String result = data.getStringExtra("result");
+            Toast.makeText(MainActivity.this, result, Toast.LENGTH_LONG).show();
+
+        }
     }
 }
--- app/src/main/java/com/example/administrator/intenttest/another.java
+++ app/src/main/java/com/example/administrator/intenttest/another.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by Administrator on 2017/9/7/007.
@@ -16,5 +18,21 @@ public class another extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.another);
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent intent=new Intent(another.this,MainActivity.class);//intent 显示启动
+                Intent intent1 = getIntent();//intent1 获取数据
+                String name = intent1.getStringExtra("name");
+                Integer age = intent1.getIntExtra("age", 20);
+                String tel = intent1.getStringExtra("tel");
+                Toast.makeText(another.this, "name:" + name + "\nage：" + age + "\ntel:" + tel, Toast.LENGTH_LONG).show();
+                intent1.putExtra("result", "name:\t" + name + " age:\t" + age + " tel:\t" + tel);
+                setResult(0, intent1);//返回结果 返回码和数据
+                finish();
+            }
+        });
+
     }
 }
